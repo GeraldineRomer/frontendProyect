ba509d85b9e21414dceb77eb8d72c98d40b30f25
GeraldineRomer <geraldine.rome0104@gmail.com>
2023-11-05 17:06:41 -0500

    login page ready

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,41 +7,45 @@ import { Button } from '@mui/material';
 
 export const Login = () => {
     return (
-        <div>
-            <div>
-                <label className='login'>Inicio Sesión</label>
-            </div>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <div className='info'>
-                    <TextField
-                    required
-                    id="outlined-password-input"
-                    label="Correo"
-                    autoComplete="current-password"
-                    />
-                    <TextField
-                    required
-                    id="outlined-password-input"
-                    label="Contraseña"
-                    type="password"
-                    autoComplete="current-password"
-                    />
+        <div className='login'>
+            <div className='login-mask'>
+                <div>
+                    <label className='login-label'>Inicio Sesión</label>
                 </div>
-            </Box>
-            <div className='btn-div-login'>
-                <Button variant="outlined" className='btn-login'>Iniciar Sesión</Button>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div className='info'>
+                        <TextField
+                        required
+                        id="outlined-password-input"
+                        label="Correo"
+                        autoComplete=""
+                        className='input'
+                        />
+                        <TextField
+                        required
+                        id="outlined-password-input"
+                        label="Contraseña"
+                        type="password"
+                        autoComplete="current-password"
+                        className='input'
+                        />
+                    </div>
+                </Box>
+                <div className='btn-div-login'>
+                    <Button variant="outlined" className='btn-login'>Iniciar Sesión</Button>
+                </div>
+                <div className='forgot-password'>
+                    <a href='#'>¿Olvidaste tu contraseña?</a>
+                </div>
             </div>
-            <div className='forgot-password'>
-                <a href='#'>¿Olvidaste tu contraseña?</a>
-            </div>
-            <Footer/>
+            <Footer className='Footer'/>
         </div>
     )
 }
